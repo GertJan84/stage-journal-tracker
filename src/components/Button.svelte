f82bd0be3194fb0cb56141de4748d9532ec9398e
Gert-Jan <gertjan@hackgreate.net>
2024-03-15 21:03:48 +0100

    frontend done and query db

---
+++ src/components/Button.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    export let text: String;
+</script>
+
+<button class="p-4 max-w-48">{text}</button>
+
+<style lang="postcss">
+    button {
+        border: 2px #6494AA solid; 
+        border-radius: 3px;
+    }
+</style>
